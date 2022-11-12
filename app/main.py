486ae46562f2a8226d08f4137bd427942b194832
Tola Awofolu <oawofolu@pivotal.io>
2022-11-12 18:25:09 -0500

    Updated MLproject parameters

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ import logging
 import time
 from scdfutils.http_status_server import HttpHealthServer
 import os
+import json
 
 HttpHealthServer.run_thread()
 logger = logging.getLogger('mlmodeltest')
@@ -16,7 +17,7 @@ logger = logging.getLogger('mlmodeltest')
 def on_send(self, _channel):
     """ Publishes data """
     logger.info("in on_send...")
-    self.channel.basic_publish(self.exchange, self.routing_key, (self.data or 'HELLO from ML Models'),
+    self.channel.basic_publish(self.exchange, self.routing_key, (json.dumps(self.data) or 'HELLO from ML Models'),
                                pika.BasicProperties(content_type='text/plain',
                                                     delivery_mode=pika.DeliveryMode.Persistent,
                                                     timestamp=int(datetime.now().timestamp())))
