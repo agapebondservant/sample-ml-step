0b57c8640547462844ad873f891be110b836ea86
Tola Awofolu <oawofolu@pivotal.io>
2023-01-21 08:10:19 -0500

    Added utility method

--- app/main.py
+++ app/main.py
@@ -26,8 +26,11 @@ dataset = None
 def process(msg):
     global dataset, buffer
     controller = ScaledTaskController.remote()
-    buffer_new = ray.data.from_items([msg.split(',')])
-    buffer = ray.data.read_csv('./data/preload.csv') if buffer is None else buffer.union(buffer_new)
+    if buffer is None:
+        logger.info("Preloading data...")
+        buffer = ray.data.from_items(utils.text_to_numpy('./data/preload.csv'))
+        logger.info("Data preloaded.")
+    buffer.union(ray.data.from_items([msg.split(',')]))
     ready = buffer.count() > (utils.get_env_var('MONITOR_SLIDING_WINDOW_SIZE') or 200)
     run_id = utils.get_env_var('MLFLOW_RUN_ID')
     experiment_id = utils.get_env_var('MLFLOW_EXPERIMENT_ID')
